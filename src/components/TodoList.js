a9a6237277417c75bc8663ecd710002893096ac7
Pablo Maribondo <pablomgvieira@gmail.com>
2021-03-27 14:41:14 -0300

    Setup do redux implemnentado

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,9 @@
+import { useSelector } from "react-redux";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
+  const todos = useSelector(state => state.todos);
+
   return (
     <div className="my-4">
       <TodoItem />
